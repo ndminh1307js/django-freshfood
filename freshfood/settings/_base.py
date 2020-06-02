6d4eef72119f3b0d5c621d14185b54927e52d30e
ndminh1307js <ndminh1307js@gmail.com>
2020-06-03 06:06:55 +0700

    Add Coupon model and apply to the shopping cart

--- freshfood/settings/_base.py
+++ freshfood/settings/_base.py
@@ -54,6 +54,7 @@ LOCAL_APPS = [
     'freshfood.apps.cart',
     'freshfood.apps.orders',
     'freshfood.apps.payment',
+    'freshfood.apps.coupons',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
